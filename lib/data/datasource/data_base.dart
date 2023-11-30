6b1cc02d66de35079a21c7dea8bb9701df0ee4db
A-RR3 <arwaarafeh@hotmail.com>
2023-11-30 08:24:34 +0200

    remove unused import

--- lib/data/datasource/data_base.dart
+++ lib/data/datasource/data_base.dart
@@ -1,6 +1,5 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:todo_app_updated/domain/entities/categories.dart';
 import 'package:todo_app_updated/utils/data_base_config.dart';
 
 class StorageService {
